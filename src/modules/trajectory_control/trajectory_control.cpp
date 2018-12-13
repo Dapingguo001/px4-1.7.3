fa957da1e9c1d9c573b0b9d73f2b8dcaddbcd6a2
yuwenbin <wenbin.yu@robsense.com>
2018-12-13 13:39:04 +0800

    add trajectory control1.2

--- src/modules/trajectory_control/trajectory_control.cpp
+++ src/modules/trajectory_control/trajectory_control.cpp
@@ -10,11 +10,18 @@
 
 #include <float.h>
 #include <mathlib/mathlib.h>
+#include <lib/geo/geo.h>
+#include <mathlib/mathlib.h>
 
 #include <uORB/uORB.h>
 #include <uORB/topics/vehicle_command.h>
 #include <uORB/topics/vehicle_control_mode.h>
 #include <uORB/topics/manual_control_setpoint.h>
+#include <uORB/topics/offboard_control_mode.h>
+#include <uORB/topics/vehicle_gps_position.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/position_setpoint_triplet.h>
+
 
 #include "trajectory_control.h"
 
@@ -33,6 +40,8 @@ TrajectoryControl::TrajectoryControl():
 {
 	memset(&_vehicle_command, 0, sizeof(_vehicle_command));
 	memset(&_sp_man, 0, sizeof(_sp_man));
+	memset(&_offboard_control_mode, 0, sizeof(_offboard_control_mode));
+	memset(&_gps_pos, 0, sizeof(_gps_pos));
 }
 
 TrajectoryControl::~TrajectoryControl()
@@ -65,14 +74,13 @@ TrajectoryControl::task_main()
 
 	//publish 消息
 	_vehicle_command_pub = orb_advertise(ORB_ID(vehicle_command), &_vehicle_command);
-
+	_offboard_control_mode_pub = orb_advertise(ORB_ID(offboard_control_mode), &_offboard_control_mode);
 	//subscribe 消息
 	_control_mode_sub = orb_subscribe(ORB_ID(vehicle_control_mode));
-
 	_sp_man_sub = orb_subscribe(ORB_ID(manual_control_setpoint));
+	_gps_pos_sub = orb_subscribe(ORB_ID(vehicle_gps_position));
+	_global_pos_sub = orb_subscribe(ORB_ID(vehicle_global_position));
     
-
-
 	while(!_task_should_exit)
 	{
 		usleep(4000);
@@ -88,27 +96,52 @@ TrajectoryControl::task_main()
         if (updated) {
 			orb_copy(ORB_ID(manual_control_setpoint), _sp_man_sub, &_sp_man);
         }
+
+        orb_check(_gps_pos_sub, &updated);
+        if (updated) {
+			orb_copy(ORB_ID(vehicle_gps_position), _gps_pos_sub, &_gps_pos);
+        }
+
+        orb_check(_global_pos_sub, &updated);
+        if (updated) {
+			orb_copy(ORB_ID(vehicle_global_position), _global_pos_sub, &_global_pos);
+        }				
+
+		if(_gps_pos.fix_type > 3 && hrt_absolute_time() < _gps_pos.timestamp + 1 * 1000 *1000)
+		{
+			_gps_valid = true;
+		}
+		else
+		{
+			_gps_valid = false;
+		}
+
 		//判断是否开启环形飞行控制任务
 		if(_sp_man.aux1 < -0.8f){
         	_start_circle_control = false;
     	}
     	if(_sp_man.aux1 >  0.8f){
        		_start_circle_control = true;
+			//初始化环形命令
+			circle_init(_circle_trajectory_command_bag);
     	}
 
 		//任务开始，开启offboard模式
-		if(_start_circle_control && !_control_mode.flag_control_offboard_enabled)
+		if(_start_circle_control && !_control_mode.flag_control_offboard_enabled && _gps_valid)
 		{
 			send_vehicle_command(true);//true :设置offboard模式  ； false:设置loiter模式
+			send_offboard_control_mode();
 		}
 
 		//判断任务是否开启，开启后开始执行任务
-		if(_start_circle_control && _control_mode.flag_control_offboard_enabled)
+		if(_start_circle_control && _control_mode.flag_control_offboard_enabled && _gps_valid)
 		{
 			_circle_control_command_start_success = true;
+
+			send_offboard_control_mode();
 		}
 
-		//任务执行结束后，切换至loiter模式
+		//任务执行结束后，gps有效，切换至loiter模式
 		if(_circle_control_command_start_success && !_start_circle_control)
 		{
 			if(!_control_mode.flag_control_auto_enabled)
@@ -118,8 +151,7 @@ TrajectoryControl::task_main()
 			else
 			{
 				_circle_control_command_start_success = false;
-			}
-			
+			}	
 		}
 	}
 }
@@ -214,6 +246,167 @@ TrajectoryControl::send_vehicle_command(bool is_offboard_mode)
 
 		orb_publish(ORB_ID(vehicle_command), _vehicle_command_pub, &_vehicle_command);		
 	}
+}
+
+void 
+TrajectoryControl::send_offboard_control_mode()
+{
+	_offboard_control_mode.ignore_thrust             = true;
+	_offboard_control_mode.ignore_attitude           = true;
+	_offboard_control_mode.ignore_bodyrate           = true;
+	_offboard_control_mode.ignore_position           = false;
+	_offboard_control_mode.ignore_velocity           = true;
+	_offboard_control_mode.ignore_acceleration_force = true;
+
+	_offboard_control_mode.timestamp = hrt_absolute_time();
+
+	orb_publish(ORB_ID(offboard_control_mode), _offboard_control_mode_pub, &_offboard_control_mode);
+}
+
+void
+TrajectoryControl::do_circle(circle_trajectory_command_bag   cir_tra_com_bag)
+{
+	struct map_projection_reference_s ref_pos;
+	//计算三维环形轨道位置转换矩阵
+	float level_2D_position[3];
+	float level_3D_position_NEU[3]; 
+	double level_3D_position_LLH[3];
+	static float theta_delta;
+	static bool calculate_cos_angle_dec = true;
+	float dt = (hrt_absolute_time() - _last_time_stamp)/(1 * 1000 * 1000);
+
+	level_2D_position[0] = cir_tra_com_bag.circle_R * cosf(cir_tra_com_bag.initial_theta + theta_delta);
+	level_2D_position[1] = cir_tra_com_bag.circle_R * sinf(cir_tra_com_bag.initial_theta + theta_delta);
+	level_2D_position[2] = 0;
+
+	//水平位置转3D位置
+	level_3D_position_NEU[0] = cir_tra_com_bag.circle_3d_tranfer_matrix[0][0] * level_2D_position[0] + cir_tra_com_bag.circle_3d_tranfer_matrix[0][1] * level_2D_position[1]
+						   		+ cir_tra_com_bag.circle_3d_tranfer_matrix[0][2] * level_2D_position[2];
+
+	level_3D_position_NEU[1] = cir_tra_com_bag.circle_3d_tranfer_matrix[1][0] * level_2D_position[0] + cir_tra_com_bag.circle_3d_tranfer_matrix[1][1] * level_2D_position[1]
+						   		+ cir_tra_com_bag.circle_3d_tranfer_matrix[1][2] * level_2D_position[2];
+
+	level_3D_position_NEU[2] = cir_tra_com_bag.circle_3d_tranfer_matrix[2][0] * level_2D_position[0] + cir_tra_com_bag.circle_3d_tranfer_matrix[2][1] * level_2D_position[1]
+						   		+ cir_tra_com_bag.circle_3d_tranfer_matrix[2][2] * level_2D_position[2];
+
+	ref_pos.lat_rad = cir_tra_com_bag.center_of_circle_lat;
+	ref_pos.lon_rad = cir_tra_com_bag.center_of_circle_lon;
+	map_projection_reproject(&ref_pos, level_3D_position_NEU[0], level_3D_position_NEU[1], 
+								&level_3D_position_LLH[0], &level_3D_position_LLH[1]);
+	level_3D_position_LLH[2] = level_3D_position_NEU[2] + cir_tra_com_bag.center_of_circle_hgt;
+
+	//计算加减速;
+	if(calculate_cos_angle_dec)
+	{
+		_cos_angle_dec = (cir_tra_com_bag.start_angle_vel * cir_tra_com_bag.start_angle_vel - 
+				cir_tra_com_bag.end_angle_vel * cir_tra_com_bag.end_angle_vel)/(cir_tra_com_bag.max_angle_acc);
+	}
+
+	if((cir_tra_com_bag.angle_sum_accomplish - theta_delta) > (_cos_angle_dec + 2/180*(float)M_PI))
+	{
+		cir_tra_com_bag.angle_vel = cir_tra_com_bag.angle_vel + cir_tra_com_bag.max_angle_acc * dt;  //初始加速阶段
+	}
+	else
+	{
+		calculate_cos_angle_dec = false;
+		if (cir_tra_com_bag.angle_vel > cir_tra_com_bag.end_angle_vel)
+		{
+			cir_tra_com_bag.angle_vel = cir_tra_com_bag.angle_vel - cir_tra_com_bag.max_angle_acc * dt;  //终止减速阶段
+		}
+		if (cir_tra_com_bag.angle_vel < 1/180*(float)M_PI)
+		{
+			cir_tra_com_bag.angle_vel = 1/180*(float)M_PI;
+		}        
+	}
+
+	if (cir_tra_com_bag.angle_vel >  cir_tra_com_bag.set_angle_vel)
+	{
+		cir_tra_com_bag.angle_vel = _last_angle_vel;
+	}
+
+	theta_delta = theta_delta + cir_tra_com_bag.angle_vel * dt;
+
+	_last_angle_vel = cir_tra_com_bag.angle_vel;
+
+	_last_time_stamp = hrt_absolute_time();
+}
+
+void
+TrajectoryControl::circle_init(circle_trajectory_command_bag   cir_tra_com_bag)
+{
+	//计算环形轨道半径
+	struct map_projection_reference_s ref_pos;
+	ref_pos.lat_rad = _global_pos.lat * M_DEG_TO_RAD;
+	ref_pos.lon_rad = _global_pos.lon * M_DEG_TO_RAD;
+	map_projection_project(&ref_pos,
+					       cir_tra_com_bag.center_of_circle_lat, cir_tra_com_bag.center_of_circle_lon,
+					       &cir_tra_com_bag.circle_R_n, &cir_tra_com_bag.circle_R_e);
+	//计算半径					   
+	cir_tra_com_bag.circle_R = sqrt(cir_tra_com_bag.circle_R_n * cir_tra_com_bag.circle_R_n + 
+									cir_tra_com_bag.circle_R_e * cir_tra_com_bag.circle_R_e);
+
+	//计算初始角度theta
+	if(cir_tra_com_bag.circle_R_n > -0.01f && cir_tra_com_bag.circle_R_n < 0.01f)
+	{
+		if(cir_tra_com_bag.circle_R_e > -0.01f && cir_tra_com_bag.circle_R_e < 0.01f)
+		{
+			cir_tra_com_bag.initial_theta = 0;
+		}
+		else if(cir_tra_com_bag.circle_R_e < 0)
+		{
+			cir_tra_com_bag.initial_theta = (float)M_PI * 3/2;
+		}
+		else
+		{
+			cir_tra_com_bag.initial_theta = (float)M_PI/2;
+		}
+	}
+	else if(cir_tra_com_bag.circle_R_n > 0)
+	{
+		if(cir_tra_com_bag.circle_R_e > -0.01f && cir_tra_com_bag.circle_R_e < 0.01f)
+		{
+			cir_tra_com_bag.initial_theta = 0;
+		}
+		else if(cir_tra_com_bag.circle_R_e < 0)
+		{
+			cir_tra_com_bag.initial_theta = atanf(cir_tra_com_bag.circle_R_n/cir_tra_com_bag.circle_R_e) + 2.0f * (float)M_PI;
+		}
+		else
+		{
+			cir_tra_com_bag.initial_theta = atanf(cir_tra_com_bag.circle_R_n/cir_tra_com_bag.circle_R_e);
+		}
+	}
+	else
+	{
+		if(cir_tra_com_bag.circle_R_e > -0.01f && cir_tra_com_bag.circle_R_e < 0.01f)
+		{
+			cir_tra_com_bag.initial_theta = (float)M_PI;
+		}
+		else
+		{
+			cir_tra_com_bag.initial_theta = atanf(cir_tra_com_bag.circle_R_n/cir_tra_com_bag.circle_R_e) + (float)M_PI;
+		}
+	}
+
+	cir_tra_com_bag.circle_3d_tranfer_matrix[0][0] = cosf(cir_tra_com_bag.circle_dip_angle_est);
+	cir_tra_com_bag.circle_3d_tranfer_matrix[0][1] = sinf(cir_tra_com_bag.circle_dip_angle_north) * sinf(cir_tra_com_bag.circle_dip_angle_est);
+	cir_tra_com_bag.circle_3d_tranfer_matrix[0][2] = -cosf(cir_tra_com_bag.circle_dip_angle_north)*sinf(cir_tra_com_bag.circle_dip_angle_est);
+	
+	cir_tra_com_bag.circle_3d_tranfer_matrix[1][0] = 0.0f;
+	cir_tra_com_bag.circle_3d_tranfer_matrix[1][1] = cosf(cir_tra_com_bag.circle_dip_angle_north);
+	cir_tra_com_bag.circle_3d_tranfer_matrix[1][2] = sinf(cir_tra_com_bag.circle_dip_angle_north);
+
+	cir_tra_com_bag.circle_3d_tranfer_matrix[2][0] = sinf(cir_tra_com_bag.circle_dip_angle_est);
+	cir_tra_com_bag.circle_3d_tranfer_matrix[2][1] = -sinf(cir_tra_com_bag.circle_dip_angle_north)*cosf(cir_tra_com_bag.circle_dip_angle_est);
+	cir_tra_com_bag.circle_3d_tranfer_matrix[2][2] = cosf(cir_tra_com_bag.circle_dip_angle_north)*cosf(cir_tra_com_bag.circle_dip_angle_est);
+
+	cir_tra_com_bag.max_angle_acc = (float)10/180*(float)M_PI;
+}
+
+void
+TrajectoryControl::tragectory_publish(circle_trajectory_command_bag   cir_tra_com_bag)
+{
+	
 }
 
 int trajectory_control_main(int argc, char *argv[])
--- src/modules/trajectory_control/trajectory_control.h
+++ src/modules/trajectory_control/trajectory_control.h
@@ -1,8 +1,12 @@
-
 #include <uORB/uORB.h>
 #include <uORB/topics/vehicle_command.h>
 #include <uORB/topics/vehicle_control_mode.h>
 #include <uORB/topics/manual_control_setpoint.h>
+#include <uORB/topics/offboard_control_mode.h>
+#include <uORB/topics/vehicle_gps_position.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/position_setpoint_triplet.h>
+
 
 class TrajectoryControl{
 public:
@@ -21,18 +25,67 @@ private:
     bool	        _task_should_exit;
 
     orb_advert_t    _vehicle_command_pub;
+    orb_advert_t    _offboard_control_mode_pub;
+    orb_advert_t    _pos_set_tri_pub;
+
     int             _control_mode_sub;
     int             _sp_man_sub;
+    int             _gps_pos_sub;
+    int             _global_pos_sub;
 
     bool            _start_circle_control = false;
     bool            _circle_control_command_start_success = false;
+    bool            _gps_valid = false;
 
-    vehicle_command_s  _vehicle_command;
-    manual_control_setpoint_s _sp_man;
+    uint64_t        _last_time_stamp;
+    float           _last_angle_vel;
+    float           _cos_angle_dec;
 
-    vehicle_control_mode_s  _control_mode;
+    vehicle_command_s           _vehicle_command;
+    offboard_control_mode_s     _offboard_control_mode;
+
+    manual_control_setpoint_s   _sp_man;
+    vehicle_control_mode_s      _control_mode;
+    vehicle_gps_position_s      _gps_pos;
+    vehicle_global_position_s   _global_pos;
+    position_setpoint_triplet_s _pos_set_tri;
+
+    struct circle_trajectory_command_bag{
+        //用户输入参数
+        float center_of_circle_lat;       //圆心所在的维度
+        float center_of_circle_lon;       //圆心所在的经度
+        float center_of_circle_hgt;       //圆心所在的高度
+        float circle_dip_angle_north;     //圆环北向倾斜角度
+        float circle_dip_angle_est;       //圆形东向倾斜角度
+        float end_angle_vel;              //终止速度
+        float set_angle_sum;              //运行总角度  
+        float set_angle_vel;              //设定运行角速度
+        uint8_t direction_of_rotation;    //运行方向 1:顺时针旋转, 0:逆时针旋转
+
+        //初始化圆环参数
+        float circle_R;
+        float circle_R_n;
+        float circle_R_e;
+        float initial_theta;
+        float max_angle_acc;
+        float circle_3d_tranfer_matrix[3][3]; 
+
+        //任务运行状态
+        bool first_carry_out_task;        //true:第一次运行程序， false:非第一次运行程序
+        bool task_complete;               //true:完成， false:未完成
+        float start_angle_vel;            //起始角速度，任务初始化时，获取飞行器当前角速度
+        float angle_vel;                  //当前时刻角速度
+        float angle_sum_accomplish;
+    };
+
+    circle_trajectory_command_bag   _circle_trajectory_command_bag;
 
     void send_vehicle_command(bool is_offboard_mode);
 
+    void send_offboard_control_mode();
 
+    void            do_circle(circle_trajectory_command_bag   cir_tra_com_bag);
+    void            do_line();
+    void            circle_init(circle_trajectory_command_bag   cir_tra_com_bag);
+    void            tragectory_publish(circle_trajectory_command_bag   cir_tra_com_bag);
 };
